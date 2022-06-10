6b3f59d41bfd966d3601f95d0a579db1f3763eba
seungmileee <dltmdal0928@naver.com>
2022-06-10 16:32:31 +0900

    level 1 / 짝수와 홀수

---
+++ Programmers/짝수와_홀수.js
@@ -0,0 +1,11 @@
+function solution(num) {
+  // 정수 num이 짝수인 경우 "Even"을, 홀수일 경우 "Odd"를 반환하는 함수를 완성하라
+  // 0은 짝수이다
+
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
