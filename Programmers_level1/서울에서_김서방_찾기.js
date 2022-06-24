5dbe6c2c2ce47bac634d5b52fa1c1213dddbf34b
seungmileee <dltmdal0928@naver.com>
2022-06-24 18:03:00 +0900

    Programmers_level1

--- Programmers_level1/서울에서_김서방_찾기.js
+++ Programmers_level1/서울에서_김서방_찾기.js
@@ -1,6 +1,6 @@
 function solution(seoul) {
   // string형 배열 seoul의 element 중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 string을 반환하라
-  // seoul에 "Kim"은 오직 한 번만 나타나며, 잘못된 값이 입력되는 경우는 없다
+  // seoul에 "Kim"은 오직 한 번만 나타나며, 잘못된 값이 입력되는 경우는 없다.
 
   return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 }
