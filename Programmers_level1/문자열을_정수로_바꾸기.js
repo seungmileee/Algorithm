01ec84db2bce08ca9cad9b83245d351a0c7798a8
seungmileee <dltmdal0928@naver.com>
2022-06-27 12:29:44 +0900

    Programmers_level1

---
+++ Programmers_level1/문자열을_정수로_바꾸기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  // 문자열 s를 숫자로 변환한 결과를 반환하라
+  // s의 길이는 1 이상 5 이하
+  // s의 맨 앞에는 부호 (+, -)가 올 수 있다
+  // s는 부호와 숫자로만 이루어져 있고 0으로 시작하지 않는다
+
+  // ex) str = "1234" -> return 1234
+  // ex) str = "-1234" -> return -1234
+
+  return Number(s);
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
