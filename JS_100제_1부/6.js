77b5908a3e3bdd09d9ab0022a247d5c68bbe8116
seungmileee <dltmdal0928@naver.com>
2022-06-29 18:05:59 +0900

    code festival

---
+++ JS_100제_1부/6.js
@@ -0,0 +1,12 @@
+// 문제 6 : False
+// 다음은 자바스크립트 문법 중에서 False로 취급하는 것들 입니다
+// 이 중 False로 취급하지 않는, True를 찾아주세요.
+
+// 1. NaN
+// 2. 1
+// 3. ""
+// 4. 0
+// 5. undefined
+
+// 정답은 2
+// Flase로 취급하는 것들 -> null, undefined, 0, 빈 문자열, NaN, false
