e2fb9f82501f5f99fadf4f230cfb83ee82ab9289
seungmileee <dltmdal0928@naver.com>
2023-01-11 10:27:08 +0900

    Programmers_level0

---
+++ Programmers_level0/두_수의_곱.js
@@ -0,0 +1,6 @@
+function solution(num1, num2) {
+  // 정수 num1, num2가 매개변수로 주어질 때, nul1과 num2를 곱한 값을 return 하도록 함수를 만들어라
+  var answer = 0;
+  return num1 * num2;
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
