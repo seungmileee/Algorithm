5d796656696a9e1e4298adce37531a8ab678f8da
seungmileee <dltmdal0928@naver.com>
2023-01-11 10:43:27 +0900

    Programmers_level0

---
+++ Programmers_level0/두_수의_차.js
@@ -0,0 +1,6 @@
+function solution(num1, num2) {
+  // 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return 하도록 함수를 완성하라.
+
+  return num1 - num2;
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
---
+++ Programmers_level0/두_수의_합.js
@@ -0,0 +1,5 @@
+function solution(num1, num2) {
+  // 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return 하도록 함수를 완성하라.
+  return num1 + num2;
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
