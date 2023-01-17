b691b07825fda68d9870a3e8749e5fae2cf7d31f
seungmileee <dltmdal0928@naver.com>
2023-01-17 13:30:33 +0900

    Programmers_level0

---
+++ Programmers/Programmers_level0/피자_나눠먹기1.js
@@ -0,0 +1,6 @@
+function solution(n) {
+  // 머쓱이네 피자 가게는 피자를 일곱조각으로 잘라서 준다.
+  // 피자를 나눠먹을 사람의 수 n이 주어질 때, 모든 사람이 피자를 한 조각 이상 먹기 위해 필요한 피자의 수를 return 하는 함수를 완성하라.
+  return Math.ceil(n / 7);
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
