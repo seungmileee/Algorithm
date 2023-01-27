a4eab285333165b6953f5234b0aba6d48c3c616a
seungmileee <dltmdal0928@naver.com>
2023-01-27 13:37:31 +0900

    Programmers_level0

---
+++ Programmers/Programmers_level0/나이_출력.js
@@ -0,0 +1,5 @@
+function solution(age) {
+  // 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 함수를 완성하라.
+  return 2022 - age + 1;
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
---
+++ Programmers/Programmers_level0/배열_뒤집기.js
@@ -0,0 +1,5 @@
+function solution(num_list) {
+  // 정수가 들어있는 배열 num_list가 매개변수로 주어진다. num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return 하도록 함수를 완성하라
+  return num_list.reverse();
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
