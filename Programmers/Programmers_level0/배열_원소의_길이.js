f2affb8e5ba0150344e411bc451383e859f8ebd5
seungmileee <dltmdal0928@naver.com>
2023-01-31 13:47:57 +0900

    Programmers_level0

---
+++ Programmers/Programmers_level0/배열_원소의_길이.js
@@ -0,0 +1,5 @@
+function solution(strlist) {
+  // 문자열 배열 strlist가 매개변수로 주어진다. strlist 각 원소의 길이를 담은 배열을 return 하도록 함수를 완성하라
+  return strlist.map((el) => el.length);
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
