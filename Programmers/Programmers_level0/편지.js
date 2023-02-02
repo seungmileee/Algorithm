4b1970815c89f5ce707a01f6ab80ea44afb57ccc
seungmileee <dltmdal0928@naver.com>
2023-02-02 23:47:07 +0900

    Programmers_level0

---
+++ Programmers/Programmers_level0/편지.js
@@ -0,0 +1,6 @@
+function solution(message) {
+  // 머쓱이는 할머니께 생신 축하 편지를 쓰려고 한다. 할머니가 보시기 편하도록 글자 한 자 한 자를 가로 2cm 크기로 적으려고 하며, 편지를 가로로만 적을 때, 축하 문구 message를 적기 위해 필요한 편지지의 최소 가로 길이를 return 하도록 함수를 완성하라.
+
+  return message.length * 2;
+}
+//출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
