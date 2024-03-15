30a6f20fed5cac89f78764233fbe869433b1c2f2
seungmileee <dltmdal0928@naver.com>
2024-03-15 21:01:24 +0900

    수_정렬하기

---
+++ 백준/수_정렬하기.js
@@ -0,0 +1,22 @@
+// N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오 (2750)
+// https://www.acmicpc.net/problem/2750
+
+let fs = require("fs");
+let input = fs.feadFileSync("/dev/stdin").toString().split("\n");
+
+let n = Number(input[0]);
+let arr = [];
+for (let i = 0; i <= n; i++) {
+  arr.push(Number(input[i]));
+}
+
+arr.sort(function (a, b) {
+  return a - b;
+});
+
+let answer = "";
+for (let i = 0; i < arr.length; i++) {
+  answer += arr[i] + "\n";
+}
+
+console.log(answer);
